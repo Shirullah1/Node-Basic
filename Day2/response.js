7688884f0cc708e8b86e6f86b3f11e99ab213a4d
Shirullah1 <shirullah1995@gmail.com>
2022-09-12 01:01:44 +0600

    Day2 commit

---
+++ Day2/response.js
@@ -0,0 +1,26 @@
+let http =require('http');
+ 
+let server = http.createServer((req,res)=>{
+   if(req.url=="/"){
+    
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write('<h1>this is home page</h1>')
+    res.end()
+
+   }else if(req.url=="/about"){
+
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write('<h1>this is about page</h1>')
+    res.end()
+
+   }else if(req.url=="/contact"){
+
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write('<h1>this is contact page</h1>')
+    res.end()
+    
+   }
+
+});
+server.listen(5050)
+console.log('server run is successful ');
